64df412c789b95111f1442eb4752633f29ff27ef
Sergey <sergeykaynoy@gmail.com>
2024-02-22 22:21:39 +0700

    new model

--- Events/core/admin.py
+++ Events/core/admin.py
@@ -1,5 +1,6 @@
 from django.contrib import admin
 
-from .models import Event
+from . import models
 
-admin.site.register(Event)
+admin.site.register(models.Event)
+admin.site.register(models.Speaker)
--- Events/core/models.py
+++ Events/core/models.py
@@ -1,6 +1,19 @@
 from django.db import models
 
 
+class Speaker(models.Model):
+    first_name = models.CharField(max_length=100)
+    last_name = models.CharField(max_length=100)
+    surname = models.CharField(max_length=100)
+
+    class Meta:
+        verbose_name = 'Спикер'
+        verbose_name_plural = 'Спикеры'
+
+    def __str__(self):
+        return f'Спикер {self.first_name} {self.last_name} {self.pk}'
+
+
 class Event(models.Model):
     title = models.CharField(max_length=255)
     description = models.TextField()
